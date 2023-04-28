00af1fb0f58fb1ddc9801f45664ba1a1f8bfdaef
Alberto Iannaccone <a.iannaccone@arduino.cc>
2023-04-28 12:17:31 +0200

    add timer

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className='app'>
-      <Header />
+      <Header gameStatus={started ? 'started' : 'initial'} />
       <div className='main'>
         {
           started ?
--- src/Header.jsx
+++ src/Header.jsx
@@ -2,24 +2,72 @@
 import './Header.css';
 import { Overlay } from './Overlay';
 import logo from './assets/nyt.svg'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useStopwatch } from 'react-timer-hook';
 
-export function Header() {
+export function Header({ gameStatus }) {
+  const {
+    seconds,
+    minutes,
+    start,
+    pause,
+  } = useStopwatch({ autoStart: false });
   const [paused, setPaused] = useState(false);
+
+  useEffect(() => {
+    if (gameStatus === 'started') {
+      start();
+    }
+    if (gameStatus === 'solved') {
+      pauseTimer();
+    }
+  }, [gameStatus]);
+
+  const pauseTimer = () => {
+    setPaused(true);
+    pause();
+  }
 
   const unpause = () => {
     setPaused(false);
+    start();
   }
 
   return <header className="header">
-    <div className='container'>
-      <button onClick={() => setPaused(true)}>pause</button>
-      <img src={logo} className="logo" alt="New York Times logo" />
-    </div>
+    <button type="button" aria-label="Navigation menu button" aria-haspopup="true" aria-expanded="false" className="pz-nav__hamburger-helper pz-nav__hamburger pz-nav__hamburger-squeeze" id="js-nav-burger">
+      <span className="pz-nav__hamburger-box">
+        <span className="pz-nav__hamburger-inner">
+        </span>
+      </span>
+    </button>
+    {gameStatus === 'started' ?
+      <>
+        <div className='timer'>
+          <button onClick={pauseTimer} className='timer-button'>
+            <div>
+              <span>{minutes}</span>:<span>{seconds.toLocaleString('en-US', {
+                minimumIntegerDigits: 2,
+                useGrouping: false
+              })}</span>
+            </div>
+            <i className='timer-button-icon'></i>
+          </button>
+        </div>
+        <div className='other-buttons'>
+          <button className='button-icon'><i className='information'></i></button>
+          <button className='button-icon'><i className='lifesaver'></i></button>
+          <button className='button-icon'><i className='settings'></i></button>
+        </div>
+      </>
+      : <>
+        <img src={logo} className="logo" alt="New York Times logo" />
+        <div className='empty-space-45'></div>
+      </>
+    }
     <Overlay visible={paused} onHide={unpause} header={"Your game is paused."} description={"Ready to play?"}>
       <button className='button' onClick={unpause}>
         Continue
       </button>
     </Overlay>
-  </header>
+  </header >
 }
